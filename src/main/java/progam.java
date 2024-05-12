0a558efafb560dcc082283082d55988827731b08
quocbao1996 <165663882+quocbao1996@users.noreply.github.com>
2024-05-13 01:28:30 +0900

    baitap

---
+++ src/main/java/progam.java
@@ -0,0 +1,11 @@
+public class progam {
+    public static void main(String[] args) {
+        Exercise04 exercise04 = new Exercise04();
+        exercise04.question01();
+        exercise04.question02();
+        exercise04.question03();
+        exercise04.question04();
+        exercise04.question05();
+        exercise04.question07();
+    }
+}
